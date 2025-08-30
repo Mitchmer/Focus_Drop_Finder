99bb8bd50f83da7c3b92e13b7696aa743a4a487f
Mitch Merrell <themitchguy@gmail.com>
2025-08-30 14:17:29 -0700

    set up API key

--- main.py
+++ main.py
@@ -3,18 +3,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+API_KEY = os.getenv("BUNGIE_API_KEY")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Add more stuff here")
+    # add main stuff
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
